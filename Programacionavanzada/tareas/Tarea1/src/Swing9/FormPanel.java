f7d5913b99a983bd18ecb2a7816b03977c86931d
LuisGuerrero1414 <megaprackpro@gmail.com>
2026-02-06 00:33:29 -0600

    Add files via upload

---
+++ Programacionavanzada/tareas/Tarea1/src/Swing9/FormPanel.java
@@ -0,0 +1,16 @@
+package Swing9;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class FormPanel extends JPanel {
+    
+    public FormPanel() {
+
+        Dimension dim = getPreferredSize();
+        
+        dim.width = 250;
+
+        setPreferredSize(dim);
+    }
+}
---
+++ Programacionavanzada/tareas/Tarea1/src/Swing9/StringListener.java
@@ -0,0 +1,6 @@
+package Swing9;
+
+
+public interface StringListener {
+ public void textEmitted(String text);
+}
