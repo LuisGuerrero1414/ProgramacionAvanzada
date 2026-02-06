38e6bc357dd0f5028ba286af397c5ae77f189fe8
LuisGuerrero1414 <megaprackpro@gmail.com>
2026-02-06 00:30:13 -0600

    Add files via upload

---
+++ Programacionavanzada/tareas/Tarea1/src/Swing4/MainFrame.java
@@ -0,0 +1,42 @@
+package Swing4;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class MainFrame extends JFrame {
+
+    private JTextArea textArea;
+    private JButton btn;
+
+    public MainFrame() {
+        super("Hello World");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Click me");
+
+        btn.addActionListener(new ActionListener() {
+            
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+
+                textArea.append("hello\n");
+            }
+        });
+
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
