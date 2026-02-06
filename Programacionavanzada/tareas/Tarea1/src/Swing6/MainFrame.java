416e15b68dcaa655f657ac6b09c30cbbb483d907
LuisGuerrero1414 <megaprackpro@gmail.com>
2026-02-06 00:31:20 -0600

    Add files via upload

---
+++ Programacionavanzada/tareas/Tarea1/src/Swing6/MainFrame.java
@@ -0,0 +1,44 @@
+package Swing6;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    
+    private TextPanel textPanel;
+    private JButton btn;
+    private Toolbar toolbar; 
+    
+    public MainFrame() {
+        super("Hello World");
+        
+        setLayout(new BorderLayout());
+        
+        toolbar = new Toolbar(); 
+        textPanel = new TextPanel();
+        btn = new JButton("Click me");
+        
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                textPanel.appendText("hello\n");
+            }
+        });
+        
+
+        add(toolbar, BorderLayout.NORTH);
+        
+
+        add(textPanel, BorderLayout.CENTER);
+
+        add(btn, BorderLayout.SOUTH);
+        
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
